814c3376c50bca7167940d2889e9cb9118dc6697
Mas'ul <masul.fauzi@gmail.com>
2025-07-28 08:47:51 +0700

    feat: ganti tampilan

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/dashboard2/testing', function () {
+    return view('dashboard2');
+})->middleware(['auth', 'verified'])->name('dashboard2');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
