83ffbd259f423bfa587c12a521dea64d70aded01
Erik <akundummy409@gmail.com>
2025-07-29 09:19:13 +0700

    perbaikan blade sidebar

--- resources/views/parts/sidebar.blade.php
+++ resources/views/parts/sidebar.blade.php
@@ -11,13 +11,13 @@
     @endphp
     @foreach ($menus->where('level', 0) as $item)
 			@if($menus->where('parent_id', $item->id)->where('level', 1)->count() > 0)
-				<li class="sidebar-title"><h6>{{ $item->menu }}</h6></li>
-				<hr>
+            <li class="menu-title"><h6>{{ $item->menu }}</h6></li>
+            <hr>
 			@endif
 			@foreach ($menus->where('parent_id', $item->id)->where('level', 1) as $menu)
 				@if ($menus->where('parent_id', $item->id)->where('level', 2)->count() > 0)
 					<li class="sidebar-item has-sub">
-						<a href="#" class='sidebar-link'> <i class="bi bi-stack"></i> <span>{{ $menu->menu }}</span> </a>
+						<a href="#" class='menu-link'> <i class="bi bi-stack"></i> <span>{{ $menu->menu }}</span> </a>
 						<ul class="submenu">
 							@foreach ($menus->where('parent_id', $item->id)->where('level', 2) as $submenu)
 								<li class="submenu-item">
@@ -27,8 +27,8 @@
 						</ul>
 					</li>
 				@else
-					<li class="sidebar-item {{ $menu->routing == $route ? 'active' : '' }}">
-						<a href="{{ route($menu->routing) }}" class='sidebar-link'>
+					<li class="menu-item {{ $menu->routing == $route ? 'active' : '' }}">
+						<a href="{{ route($menu->routing) }}" class='menu-link'>
 							<i class="fa {{ $menu->icon }}"></i> <span>{{ $menu->menu }}</span>
 						</a>
 					</li>
