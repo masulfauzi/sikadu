6e3519c01bb22c7446903f1bb069bf1e5edd512f
Erik <akundummy409@gmail.com>
2025-08-27 10:10:12 +0700

    fix: tampilan halaman import excel

--- app/Modules/Siswa/Views/siswa_upload.blade.php
+++ app/Modules/Siswa/Views/siswa_upload.blade.php
@@ -28,28 +28,33 @@
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="header-title">Form Import Data {{ $title }}</h4>
+                            {{-- <h4 class="header-title">Form Import Data {{ $title }}</h4> --}}
                              <div class="row">
-                                <div class="col-3">
+                                <div class="col-5">
                                 </div>
-                                <div class="col-7">
+                                <div class="col-5">
                                 </div>
-                                <div class="col-2">
+                                <div class="col-2 mt-3">
                                     <a href="#" class="btn btn-primary">Panduan Format File</a>
                                 </div>
                             </div>
-                            
                         </div>
-                        
+                        <hr>
                         <div class="card-body">
                             <form class="form form-horizontal" action="{{ route('siswa.import.excel') }}" method="POST" enctype="multipart/form-data">
                                 <div class="form-body">
                                     @csrf 
+                                    <div class="row">
+                                    <div class="col-3">
                                     <label>Pilih File Excel : </label>
                                     <input type="file" name="excel" required>
+                                    </div>
+                                    <div class="col-9">
                                     <div class="offset-md-3 ps-2">
                                         <button class="btn btn-primary" type="submit" name="submit">Simpan</button> &nbsp;
                                         <a href="{{ route('siswa.index') }}" class="btn btn-secondary">Batal</a>
+                                    </div>
+                                    </div>
                                     </div>
                                 </div>
                             </form>
